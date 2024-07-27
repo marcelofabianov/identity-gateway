380d5b72014c0ccef9355cca98d58415ad4f1fb7
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-27 19:40:08 -0300

    chore: criar mock de realm_repository

---
+++ internal/port/outbound/mock/realm_repository.go
@@ -0,0 +1,18 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+
+	"github.com/marcelofabianov/identity-gateway/internal/port/outbound"
+)
+
+type MockCreateRealmRepository struct {
+	mock.Mock
+}
+
+func (m *MockCreateRealmRepository) Create(ctx context.Context, input outbound.CreateRealmRepositoryInput) error {
+	args := m.Called(ctx, input)
+	return args.Error(0)
+}
