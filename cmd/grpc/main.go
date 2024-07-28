70b9bf093282d6591a77d4da4fd8422259b749f5
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-28 17:25:13 -0300

    chore: iniciar novo cmd/grpc/main.go

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,16 +1,47 @@
 package main
 
-import "google.golang.org/grpc"
+import (
+	"context"
+	"log"
+
+	"github.com/marcelofabianov/identity-gateway/config"
+	"github.com/marcelofabianov/identity-gateway/pkg/logger"
+	"github.com/marcelofabianov/identity-gateway/pkg/postgres"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	//
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+
+	logger, err := logger.NewLogger(cfg.Log)
+	if err != nil {
+		log.Fatalf("error creating logger: %v", err)
+	}
+	defer logger.Close()
+
+	ctx := context.Background()
+
+	db, err := postgres.Connect(ctx, cfg.Db)
+	if err != nil {
+		logger.Fatal("error connecting to database", logger.FieldError(err))
+	}
+	defer func() {
+		if err := db.Close(ctx); err != nil {
+			log.Fatal("error closing database connection")
+		}
+	}()
+
+	logger.Info("starting application")
 }
 
 func grpcServer(host string, port string) {
 	//...
 }
 
-func loadTLSCrendentials(certPath string, keyPath string) {
+func loadTlsCrendentials(certPath string, keyPath string) {
 	//...
 }
 
